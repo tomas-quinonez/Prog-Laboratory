ee56324ab29ac59a45e5a5bdbb43f240d68cbfa9
tomasq <tomasq@tomasq-Lenovo-ideapad-100-14IBY>
2019-11-27 14:55:03 -0300

    se agregaron detalles a las clases celula y main

--- src/TP2Concurrente/Celula.java
+++ src/TP2Concurrente/Celula.java
@@ -10,17 +10,88 @@ public class Celula {
     private int posCol;
     private LinkedList<Celula> listaAdy;
     
-    public Celula(Celula[][] matriz,int estado, int posFila, int posCol) {
+    public Celula(Celula[][] m,int estado, int posFila, int posCol) {
         this.estado=estado;
         this.siguienteEstado=0;
         this.posFila=posFila;
         this.posCol=posCol;
         this.listaAdy=new LinkedList<Celula>();
-        this.cargarListaAdy(matriz);
+        this.cargarListaAdy(m);
     }
     
     public void cargarListaAdy(Celula[][] m){
-        
+        int k=this.posFila, l=this.posCol;
+        //vecino superior
+        if(this.posFila-1<0){
+            k=m.length-1;
+        }else{
+            k=this.posFila-1;
+        }
+        this.listaAdy.add(m[k][this.posCol]);
+        //
+        //vecino inferior
+        if(this.posFila+1>m.length-1){
+            k=0;
+        }else{
+            k=this.posFila+1;
+        }
+        this.listaAdy.add(m[k][this.posCol]);
+        //
+        //vecino izquierdo
+        if(this.posCol-1>0){
+            k=m[0].length-1;
+        }else{
+            k=this.posCol-1;
+        }
+        this.listaAdy.add(m[this.posFila][k]);
+        //
+        //vecino derecho
+        if(this.posCol+1>m[0].length-1){
+            k=0;
+        }else{
+            k=this.posCol+1;
+        }
+        this.listaAdy.add(m[this.posFila][k]);
+        //
+        //vecino diagonal sup izquierdo
+        if(this.posFila+1>0 && this.posCol<0){
+            k=m.length-1;
+            l=m[0].length-1;
+        }else{
+            k=this.posFila-1;
+            l=this.posCol-1;
+        }
+        this.listaAdy.add(m[k][l]);
+        //
+        //vecino diag sup derecho
+        if(this.posFila-1<0 && this.posCol+1>m.length-1){
+            k=m.length-1;
+            l=0;
+        }else{
+            k=this.posFila-1;
+            l=this.posCol+1;
+        }
+        this.listaAdy.add(m[k][l]);
+        //
+        //vecino diagonal inferior izq
+        if(this.posFila+1>m.length-1 && this.posCol-1<0){
+            k=0;
+            l=m[0].length-1;
+        }else{
+            k=this.posFila+1;
+            l=this.posCol-1;
+        }
+        this.listaAdy.add(m[k][l]);
+        //
+        //vecino diagonal inferior derecho
+        if(this.posFila+1>m.length-1 && this.posCol+1>m[0].length-1){
+            k=0;
+            l=0;
+        }else{
+            k=this.posFila+1;
+            l=this.posCol+1;
+        }
+        this.listaAdy.add(m[k][l]);
     }
     
     public void verificarVecinos(){
@@ -57,5 +128,12 @@ public class Celula {
     
     public int obtenerEstadoActual(){
         return this.estado;
+    }
+    
+    public char mostrarCelula(){
+        char cell='+';
+        if(this.estado==1)
+            cell='+';
+        return cell;
     }
 }
--- src/TP2Concurrente/MainClass.java
+++ src/TP2Concurrente/MainClass.java
@@ -1,17 +1,44 @@
 package TP2Concurrente;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainClass {
 
     public static void main(String[] args) {
 
-        char[][] m = new char[30][30];
-        
-
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                System.out.print("  +");	// Imprime elemento
-            }
-            System.out.println();	// Imprime salto de línea
+        try {
+            Celula[][] m = new Celula[30][30];
+            
+            CyclicBarrier esperarVerificacion = new CyclicBarrier(3);
+            CyclicBarrier esperarCambioEstado = new CyclicBarrier(4);
+            
+            ExecutorService poolThreads=Executors.newFixedThreadPool(3);
+            poolThreads.execute(new HiloSubmundo(m, 0, 9, esperarVerificacion, esperarCambioEstado));
+            poolThreads.execute(new HiloSubmundo(m, 10, 19, esperarVerificacion, esperarCambioEstado));
+            poolThreads.execute(new HiloSubmundo(m, 20, 29, esperarVerificacion, esperarCambioEstado));
+            
+            while(true){
+                esperarCambioEstado.await();
+                imprimirMundo(m);
+            }    
+        } catch (InterruptedException ex) {
+            Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BrokenBarrierException ex) {
+            Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public static void imprimirMundo(Celula[][] mundo){
+        for (int i = 0; i < mundo.length; i++) {
+                for (int j = 0; j < mundo[i].length; j++) {
+                    System.out.print("  +");	// Imprime elemento
+                }
+                System.out.println();	// Imprime salto de línea
+            }
     }
 }
